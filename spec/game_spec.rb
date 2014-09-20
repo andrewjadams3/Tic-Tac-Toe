505a8a469b95eff86bb808bf7d966570409f508e
Andrew Adams <andrewjadams3@gmail.com>
2014-09-20 13:54:33 -0500

    Initialization tests for new game

---
+++ spec/game_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require_relative '../game'
+
+describe Game do
+
+  describe 'new game' do
+    let(:game) { Game.new }
+
+    it 'should have an empty board' do
+      expect(game.empty?).to be true
+    end
+
+    it 'should set ai and player symbols' do 
+      expect(game.ai_symbol).to eq 'O'
+      expect(game.player_symbol).to eq 'X'
+    end
+
+    it 'should allow the player to go first' do
+      expect(game.current_turn).to eq 'X'
+    end
+
+    it 'should not have a winner' do
+      expect(game.winner?).to be false
+    end
+
+  end
+
+end
