47c11fc6eed89fc7f3f2f0ee35ec341f3bdc4d71
Andrew Adams <andrewjadams3@gmail.com>
2014-09-21 19:25:05 -0500

    Add insults, better instructions

--- view.rb
+++ view.rb
@@ -1,8 +1,19 @@
 class View
-  def initialize(game)
-    @game = game
-    @position = 0
-    @board = <<BOARD
+  INSULTS = [
+    "I once owned a dog that was smarter then you.",
+    "You make me want to puke.",
+    "You play like a dairy farmer.",
+    "Have you stopped wearing diapers yet?",
+    "I've heard you were a contemptible sneak.",
+    "You're no match for my brains, you poor fool.",
+    "There are no words for how disgusting you are.",
+    "You are a pain in the backside, sir!",
+    "There are no clever moves that can help you now.",
+    "I'm not going to take your insolence sitting down!",
+    "I usually see people like you passed-out on tavern floors."
+  ]
+
+  BOARD = <<BOARD
 
  X | X | X 
 -----------
@@ -11,6 +22,16 @@ class View
  X | X | X 
 
 BOARD
+
+  def initialize(game)
+    @game = game
+    @position = 0
+    @current_insult = INSULTS.sample
+    @board = BOARD
+  end
+
+  def draw_screen
+    [draw_board, status_line, position_line, instructions].join("\n")
   end
 
   def draw_board
@@ -18,12 +39,8 @@ BOARD
     @board.gsub(" X ") do
       index += 1
       field = @game.board[index] ? @game.board[index] : " "
-      @position == index ? "[#{field}]" : " #{field} "
+      @position == index ? "(#{field})" : " #{field} "
     end
-  end
-
-  def draw_screen
-    [draw_board, status_line, position_line, "Q = Quit | R = Reset | Return = Play"].join("\n")
   end
 
   def move(x,y)
@@ -34,6 +51,11 @@ BOARD
 
   def set
     @game.make_move(@position)
+    @current_insult = INSULTS.sample
+  end
+
+  def instructions
+    "Q = Quit  | Return = Play\nR = Reset | Arrow Keys = Move"
   end
 
   def position_line
@@ -45,8 +67,10 @@ BOARD
       "'#{@game.winner}' wins! Press 'R' to reset."
     elsif @game.over?
       "It's a draw... Press 'R' to reset."
-    else
+    elsif @game.number_of_moves == 0
       "Let's play!"
+    else
+      @current_insult
     end
   end
 end
