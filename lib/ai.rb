b319a47b48d588d10fd307f5c168f5bd97f7da13
Andrew Adams <andrewjadams3@gmail.com>
2014-09-25 08:18:00 -0500

    Rename and refactor AI methods for clarity

--- lib/ai.rb
+++ lib/ai.rb
@@ -17,21 +17,15 @@ class Ai
     if @game.number_of_moves == 1
       chosen_move = @board.center_taken? ? @board.random_corner : @board.center
     else
-      predict_move(@game)
+      find_best_choice(@game)
       chosen_move = @best_choice
     end
     chosen_move
   end
 
-  def predict_move(game)
+  def find_best_choice(game)
     return score(game) if game.over?
-    moves = Hash.new
-
-    game.available_moves.each do |position|
-      test_game = game.dup
-      test_game.make_move(position)
-      moves[position] = predict_move(test_game)
-    end
+    moves = try_moves(game)
 
     if game.current_turn == @ai_symbol
       max_score = moves.max_by{|position, score| score}
@@ -42,6 +36,16 @@ class Ai
       @best_choice = min_score.first
       return min_score.last
     end
+  end
+
+  def try_moves(game)
+    moves = Hash.new
+    game.available_moves.each do |position|
+      test_game = game.dup
+      test_game.make_move(position)
+      moves[position] = find_best_choice(test_game)
+    end
+    moves
   end
 
   WIN  =  1
